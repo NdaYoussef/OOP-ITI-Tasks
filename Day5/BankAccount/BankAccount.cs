95ce750367f624d2d5a0159cda538655c1a99b96
NdaYoussef <ndayou99@gmail.com>
2026-01-18 10:28:39 +0200

    edit and complete task Day5/BankAccount/BankAccount

--- Day5/BankAccount/BankAccount.cs
+++ Day5/BankAccount/BankAccount.cs
@@ -66,7 +66,7 @@ namespace BankAccount
             if (amount > _balance)
                 return "Insufficient Balance!";
 
-            _balance   -= amount;
+            _balance -= amount;
             return $"New Balance = {_balance}";
         }
 
